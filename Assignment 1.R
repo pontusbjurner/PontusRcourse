f1b56b6f6966a6b85edfcef87f1c920e018b9e97
pontusbjurner <pontus.bjurner@icloud.com>
2024-11-18 16:45:14 +0100

    Adding Assignment 1,

---
+++ Assignment 1.R
@@ -0,0 +1,39 @@
+## Assignment 1 ##
+
+#3. Installing and Initializing Packages:
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("renv")
+renv::snapshot() 
+
+# 4. Loading and Exploring Data:
+data(iris)
+head(iris)
+summary(iris)
+str(iris)
+
+#5. Data Manipulation:
+# create tibble format table
+df <- tbl_df(iris) 
+df
+iris <- as_tibble(iris)
+df <- iris
+filter(df, Species == "versicolor")
+filter(df, Petal.Length > 2)
+filter(df, Petal.Length > 6 & Sepal.Length > 7)       
+arrange(df, Sepal.Length, Petal.Width)
+arrange(df, desc(Sepal.Length))
+select(df, Species, Petal.Width, Petal.Length)
+mutate(df, log.Sepal.length = log(Sepal.Length))
+summarise(df, mean(Petal.Length))
+group_by(df, Species) %>% count(n())
+df %>% 
+  group_by(Species) %>%
+  summarise(mean(Petal.Length))
+
+#6. Data Visualization with ggplot2:
+ggplot(data=df, aes(x = Sepal.Length, y = Sepal.Width))+
+  geom_point(aes(color=Species, shape=Species)) +
+  xlab("Sepal Length") +
+  ylab("Sepal Width") +
+  ggtitle("Sepal Length-Width")
